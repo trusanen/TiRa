35912ae1a37d591356cbb0a673b7594ebddeeb7a
Topias <topias@Koodibuntu.(none)>
2013-05-17 19:54:58 +0300

    Matriisiluokka kayttovalmis

---
+++ DummyEngine/build/Debug/GNU-Linux-x86/tests/tests/matrixTest.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux-x86/tests/tests/matrixTest.o: tests/matrixTest.c \
+ matrix.h
+
+matrix.h:
---
+++ DummyEngine/matrixTest.c
@@ -0,0 +1,33 @@
+#include "matrix.h"
+
+void matrixTest() {
+    
+    matrix* A = newMatrix(3,3);
+    matrix* B = newMatrix(3,3);
+    
+    A->values[0][2] = 1.1;
+    B->values[2][0] = 1.1;
+    
+    printMatrix(A);
+    printMatrix(B);
+    
+    matrix* C = matrixSum(A,B);
+    
+    printMatrix(C);
+    
+    matrix* D = matrixMultiply(A,B);
+    
+    printMatrix(D);
+    
+    matrix* E = newMatrix(5, 5);
+    
+    matrixFill(E, 5.0);
+    
+    printMatrix(E);
+    
+    deleteMatrix(A);
+    deleteMatrix(B);
+    deleteMatrix(C);
+    deleteMatrix(D);
+    deleteMatrix(E);
+}
